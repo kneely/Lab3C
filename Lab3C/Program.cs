4eb917fad626a096816e337775c57e42a1004638
xkevinx45 <kneely1995@gmail.com>
2018-04-07 16:18:14 -0400

    First Commit

--- Lab3C/Program.cs
+++ Lab3C/Program.cs
@@ -1,15 +1,90 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lab3C
 {
     class Program
     {
+        private static decimal a, b, result;
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter the mathmatical operator for the function you would like to use.");
+            Int16 input = Convert.ToInt16(Console.ReadLine());
+            switch (input)
+            {
+                case 1:
+                    {
+                        Add();
+                        break;
+                    }
+                case 2:
+                    {
+                        Subtract();
+                        break;
+                    }
+                case 3:
+                    {
+                        Multiply();
+                        break;
+                    }
+                case 4:
+                    {
+                        Divide();
+                        break;
+                    }
+            }
+        }
+
+        static void Add()
+        {
+            Console.WriteLine("Please enter the first number.");
+            a = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Please enter the second number.");
+            b = Convert.ToDecimal(Console.ReadLine());
+
+            result = (a + b);
+
+            Console.WriteLine("The sum of {0} and {1} is {2}.", a, b, result);
+        }
+
+        static void Subtract()
+        {
+            Console.WriteLine("Please enter the first number.");
+            a = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Please enter the second number.");
+            b = Convert.ToDecimal(Console.ReadLine());
+
+            result = (a - b);
+
+            Console.WriteLine("{0} subtracted by {1} is {2}.", a, b, result);
+        }
+
+        static void Multiply()
+        {
+            Console.WriteLine("Please enter the first number.");
+            a = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Please enter the second number.");
+            b = Convert.ToDecimal(Console.ReadLine());
+
+            result = (a * b);
+
+            Console.WriteLine("{0} multiplied by {1} is {2}", a, b, result);
+        }
+
+        static void Divide()
+        {
+            Console.WriteLine("Please enter the first number.");
+            a = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Please enter the second number.");
+            b = Convert.ToDecimal(Console.ReadLine());
+
+            result = (a / b);
+
+            Console.WriteLine("{0} divided by {1} is {2}", a, b, result);
         }
     }
 }
